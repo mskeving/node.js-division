25ecb3bb3261935c66d1e57055db625387aaa99e
Melissa Skevington <mskeving@gmail.com>
2014-02-13 09:50:16 -0800

    separate html template

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,20 @@
 var exec = require('child_process').exec;
 var querystring = require('querystring');
+var fs = require('fs');
 
 function start(response, postData){
     console.log("Request handler 'start' was called");
+
+    response.render('./index.html');
+    
+    fs.readFile('./index.html', function (err, html) {
+        if (err) {
+            throw err;
+        }
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(html);
+        response.end();
+    });
 
     var body = '<html>'+
      '<head>'+
@@ -18,21 +30,22 @@ function start(response, postData){
      '</body>'+
      '</html>';
 
-    response.writeHead(200, {'Content-Type': 'text.plain'});
-    response.write(body);
-    response.end();
 }
 
 function upload(response, postData) {
     console.log("Request handler 'upoad' was called.");
     response.writeHead(200, {'Content-Type': 'text.plain'});
     responseText = querystring.parse(postData).text;
-    
-    if (isNaN(responseText[0]) || isNaN(responseText[1])){
+    var numerator = responseText[0];
+    var denomenator = responseText[1];
+
+    if (isNaN(numerator) || isNaN(denomenator)){
         response.write('only numbers please!');
     } else {
-    response.write('you submitted: ' + responseText[0] +
-        ' and ' + responseText[1]);
+        quotient = numerator/denomenator;
+        response.write('you submitted: ' + responseText[0] +
+            ' and ' + responseText[1]);
+        response.write('quotient: ' + quotient);
     }
     response.end();
 }
